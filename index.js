3280290daa150aed754b00948634a9f1ba89e3a6
jsr <jaskomal2@gmail.com>
2020-10-13 18:53:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  var str = '';
+  for(var i = 0; i < dwarves.length; i++) {
+    str +=`${i + 1}. ${dwarves[i]} `;
+  }
+  return str;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = [];
+  
+  for(var i = 0; i < planeteerCalls.length; i++) {
+    arr.push(planeteerCalls[i].toUpperCase() +'!');
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+    if(words[i].length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  
+  for(var i = 0; i < foods.length; i++) {
+    if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i === 'camembert']) {
+       return foods[i];
+    }
+  }
+  return 'no cheese!';
 }
